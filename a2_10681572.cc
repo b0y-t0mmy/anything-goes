a8232b11501fb6818c51ab3ccf6ea7ba31803574
b0y-t0mmy <42819455+b0y-t0mmy@users.noreply.github.com>
2018-08-29 16:21:32 -0930

    Add files via upload

---
+++ a2_10681572.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+ int number:
+ cout << "Please enter a number \n";
+ cin >> number;
+
+      if(number%2 == 0 && number > 2)
+{cout << "It is not a prime number"<<endl;
+      }
+      else{
+      cout << "It is a prime number"<<endl;
+      }
+
+      return 0;
+}
